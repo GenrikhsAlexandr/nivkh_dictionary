7c3c9dc4c420884b4176e699595b48bcafb11252
aleksandrgenrikhs <aleksandr.genrikhs@gmail.com>
2024-01-04 16:03:42 +0300

    add audioPath

--- app/src/main/java/com/aleksandrgenrikhs/nivkhdictionary/data/WordMapper.kt
+++ app/src/main/java/com/aleksandrgenrikhs/nivkhdictionary/data/WordMapper.kt
@@ -11,24 +11,41 @@ const val HOST_URL = "http://bibl-nogl-dictionary.ru/data"
 
 class WordMapper @Inject constructor() {
     fun mapToWord(wordDto: WordDto): Word? {
-        if (wordDto.id.isNullOrBlank() || wordDto.nv.isNullOrBlank() || wordDto.ru.isNullOrBlank() || wordDto.en.isNullOrBlank()) return null
         return Word(
-            id = wordDto.id.toString(),
-            locales = mapOf(
-                Language.NIVKH.code to LocaleData(
-                    Language.NIVKH,
-                    wordDto.nv,
-                    "$HOST_URL/audio/${wordDto.id}.mp3"
-                ),
-                Language.RUSSIAN.code to LocaleData(Language.RUSSIAN, wordDto.ru),
-                Language.ENGLISH.code to LocaleData(Language.ENGLISH, wordDto.en),
-            )
+            id = wordDto.id ?: return null,
+            locales = buildMap {
+                put(
+                    "nv", LocaleData(
+                        locale = Language.NIVKH,
+                        value = wordDto.nv ?: return null
+                    )
+                )
+                wordDto.ru?.let {
+                    put(
+                        "ru", LocaleData(
+                            locale = Language.RUSSIAN,
+                            value = it
+                        )
+                    )
+                }
+                wordDto.en?.let {
+                    put(
+                        "en", LocaleData(
+                            locale = Language.ENGLISH,
+                            value = it
+                        )
+                    )
+                }
+            }
         )
     }
 
     fun mapWordDbFavoritesToWord(wordDbFavorites: WordDbFavorites): Word {
         val localesMap = mutableMapOf<String, LocaleData>()
-        localesMap["nv"] = LocaleData(Language.NIVKH, wordDbFavorites.nv)
+        localesMap["nv"] = LocaleData(
+            Language.NIVKH, wordDbFavorites.nv, "$HOST_URL/audio/${wordDbFavorites.id}.mp3"
+
+        )
         wordDbFavorites.ru?.let {
             localesMap["ru"] = LocaleData(Language.RUSSIAN, it)
         }
@@ -42,12 +59,15 @@ class WordMapper @Inject constructor() {
         id = word.id,
         nv = word.locales["nv"]?.value ?: "",
         ru = word.locales["ru"]?.value ?: "",
-        en = word.locales["en"]?.value ?: ""
+        en = word.locales["en"]?.value ?: "",
     )
 
     fun mapAllWordsDbToWord(allWordsDb: AllWordsDb): Word {
         val localesMap = mutableMapOf<String, LocaleData>()
-        localesMap["nv"] = LocaleData(Language.NIVKH, allWordsDb.nv)
+        localesMap["nv"] = LocaleData(
+            Language.NIVKH, allWordsDb.nv,
+            "$HOST_URL/audio/${allWordsDb.id}.mp3"
+        )
         allWordsDb.ru?.let {
             localesMap["ru"] = LocaleData(Language.RUSSIAN, it)
         }
